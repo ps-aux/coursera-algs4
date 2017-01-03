6ddfc5289492186c936d693ad80a5e8b1177c158
ps-aux <seriouz.accz@gmail.com>
2017-01-03 20:01:01 +0100

    Prepare week 5

---
+++ src/main/java/week5/KdTree.java
@@ -0,0 +1,51 @@
+package week5;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class KdTree {
+    // construct an empty set of points
+    public KdTree() {
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    // number of points in the set
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+
+        throw new UnsupportedOperationException();
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+    }
+}
---
+++ tools/build-week-5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+week=week5
+name=kdtree
+files=(PointSET KdTree)
+
+source build.sh
+
